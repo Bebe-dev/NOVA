80db3ed61fb1702172ccbe9cc96d76e803acde01
MIRIAMMILDRED <miriammidred20@gmail.com>
2024-06-10 14:44:11 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,29 @@ import SignUp from './pages/signup'
 import SetUp from './pages/setup'
 import EmailVerification from './pages/emailVerification'
 import Roles from './pages/roles'
+import Login from './pages/login'
+import LandingPage from './pages/landingPage'
+
+
 
 function App() {
-
   return (
-    <>
-     <BrowserRouter>
-       <Routes>
-        <Route path='/signup' element={<SignUp />} />
-        <Route path='/setup' element={<SetUp/>} />
-        <Route path='/emailverification' element={<EmailVerification/>} />
-        <Route path='/roles' element={<Roles/>} />
-       </Routes>
-     </BrowserRouter>
-    </>
+
+      <BrowserRouter>
+        <Routes>
+          <Route path='/signup' element={<SignUp />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/setup' element={<SetUp />} />
+          <Route path='/emailverification' element={<EmailVerification />} />
+          <Route path='/roles' element={<Roles />} />
+          <Route path='/landingpage' element={<LandingPage />} />
+              
+
+   
+        </Routes>
+      </BrowserRouter>
+  
   )
 }
 
-export default App
+export default App;
